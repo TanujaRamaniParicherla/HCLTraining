733cf1349c1eb905f8ee61a50e33ee886155c3c9
TanujaRamaniParicherla <74548748+TanujaRamaniParicherla@users.noreply.github.com>
2020-11-24 20:08:21 +0530

    Add files via upload

---
+++ ExtraProject6/src/org/extra/ExtraType.java
@@ -0,0 +1,23 @@
+package org.extra;
+
+public class ExtraType {
+	String name;
+	long runs;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getRuns() {
+		return runs;
+	}
+
+	public void setRuns(long runs) {
+		this.runs = runs;
+	}
+
+}
